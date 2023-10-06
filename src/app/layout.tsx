b6b507d6b0720389166963b38fab3164b62f6419
Andrew Stanton <andrew@toddprod.com>
2023-10-06 09:51:33 -0400

    Added script for google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,12 @@
-import React from "react"
-import "./globals.css"
-// eslint-disable-next-line camelcase
-import { Bebas_Neue, Roboto } from "next/font/google"
 import type { Metadata } from "next"
+import { Bebas_Neue as Bebas, Roboto } from "next/font/google"
+import Script from "next/script"
+import React from "react"
+
+import "./globals.css"
 
 //  Figma - font-family: Formula Condensed;
-// eslint-disable-next-line camelcase
-const bebas_neue = Bebas_Neue({
+const babas = Bebas({
   weight: "400",
   subsets: ["latin"],
   variable: "--font-bebas-neue",
@@ -32,9 +32,31 @@ interface RootLayoutProps {
 
 const RootLayout: React.FC<RootLayoutProps> = (props) => {
   const { children } = props
+
+  const { NEXT_PUBLIC_GA_CODE } = process.env
+
   return (
-    // eslint-disable-next-line camelcase
-    <html lang="en" className={`${bebas_neue.variable} ${roboto.variable}`}>
+    <html lang="en" className={`${babas.variable} ${roboto.variable}`}>
+      {NEXT_PUBLIC_GA_CODE && (
+        <>
+          <Script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${NEXT_PUBLIC_GA_CODE}`}
+          />
+
+          <Script id="google-analytics">
+            {`
+           window.dataLayer = window.dataLayer || [];
+           function gtag(){dataLayer.push(arguments);}
+           gtag('js', new Date());
+           gtag('config', '${NEXT_PUBLIC_GA_CODE}', {
+           page_path: window.location.pathname,
+           });
+        `}
+          </Script>
+        </>
+      )}
+
       <body>{children}</body>
     </html>
   )
